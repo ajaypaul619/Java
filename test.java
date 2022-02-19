72e8f59545051048838bb71b4476cdc0a7f727d6
ajaypaul619 <48088059+ajaypaul619@users.noreply.github.com>
2022-02-19 20:47:59 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,21 +3,7 @@ public class test {
 
     public static void main(String[] args) {
 
-        String sample="school dreeed";
-        char[] cararray=sample.toCharArray();
-        for(int i=0;i<sample.length();i++){
-        	for(int j=i+1;j<sample.length();j++){
-        		
-        		if(cararray[i]==cararray[j]){
-        			System.out.println(cararray[j]);
-        			break;
-        		}
-        	}
-        	
-        }
-        	
-        		
-        	
+      System.out.println("Hello world");
         
     }
 }
